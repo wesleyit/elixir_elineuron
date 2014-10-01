91d3caac844481ea405db946d40aa4b61d747eee
Victor Oliveira Nascimento <victor.olinasc@gmail.com>
2014-10-01 19:17:05 -0300

    Added simplest test just to check it returns numbers

--- test/elineuron_test.exs
+++ test/elineuron_test.exs
@@ -1,7 +1,10 @@
 defmodule ElineuronTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "should return a number" do
+    assert Elineuron.neuron(:linear, [2, 3, 4, 5]) |> is_number
+    assert Elineuron.neuron(:logistic, [2, 3, 4, 5]) |> is_number
+    assert Elineuron.neuron(:hyperbolic, [2, 3, 4, 5]) |> is_number
   end
+
 end
